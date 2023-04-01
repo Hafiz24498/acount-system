a98acb4c47beb85fae716abf440495f08f3ca41f
Hafiz24498 <hafiz.pangteh@live.iium.edu.my>
2023-04-01 14:20:51 +0700

    setup

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,19 @@
+import { Layout, Menu } from "antd";
+import { Header } from "antd/es/layout/layout";
+import React from "react";
+import "./style.css";
+
+const LoginPage = () => {
+  return (
+    <nav className="navbar bg-body-tertiary">
+    <div className="container-fluid">
+      <a className="navbar-brand">Navbar</a>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  );
+};
+export default LoginPage;
